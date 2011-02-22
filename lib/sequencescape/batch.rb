03ccf2fb76a456b509a25c8854d3100e20dc25ae
Matthew Denner <md12@sanger.ac.uk>
2011-02-22 11:38:26 +0000

    Added the complete & release actions to a batch

--- lib/sequencescape/batch.rb
+++ lib/sequencescape/batch.rb
@@ -4,4 +4,7 @@ class Sequencescape::Batch < ::Sequencescape::Api::Resource
   belongs_to  :pipeline
   has_many    :requests, :disposition => :inline
   composed_of :user
+
+  has_update_action :complete!, :action => 'complete'
+  has_update_action :release!, :action => 'release'
 end
