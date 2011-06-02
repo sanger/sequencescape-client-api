2458de121ea4a654f2c945f821066a3ee5466bcd
Maxime Bourget <mb14@sanger.ac.uk>
2011-06-02 12:02:50 +0100

    Add swipecard attributes to User

--- lib/sequencescape/user.rb
+++ lib/sequencescape/user.rb
@@ -1,5 +1,7 @@
-class Sequencescape::User
+class Sequencescape::User < ::Sequencescape::Api::Resource
   include Sequencescape::Api::Composition::Target
 
   attribute_accessor :login
+  attribute_writer :swipecard_code
+  attribute_reader :has_a_swipecard_code
 end
