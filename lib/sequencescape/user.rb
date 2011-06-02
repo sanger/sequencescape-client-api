4650e7e895b2017aefc7bfa7b13d8fc59381877a
Maxime Bourget <mb14@sanger.ac.uk>
2011-06-02 15:50:52 +0100

    Add missing field to User.

--- lib/sequencescape/user.rb
+++ lib/sequencescape/user.rb
@@ -1,5 +1,9 @@
 class Sequencescape::User < ::Sequencescape::Api::Resource
-  attribute_accessor :login
+  attribute_reader :login
+  attribute_accessor :email
+  attribute_accessor :first_name
+  attribute_accessor :last_name
+  attribute_accessor :barcode
 
   #TODO make swipecard_code readonly. Can't at the moment because of a bug
   #attribute_writer :swipecard_code
