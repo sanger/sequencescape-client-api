fa729b61110ba1153be67e11b274efb757565607
Maxime Bourget <mb14@sanger.ac.uk>
2011-06-02 15:36:32 +0100

    Remove readonly constraint on User#swipecard_code.

--- lib/sequencescape/user.rb
+++ lib/sequencescape/user.rb
@@ -1,7 +1,9 @@
 class Sequencescape::User < ::Sequencescape::Api::Resource
-  include Sequencescape::Api::Composition::Target
-
   attribute_accessor :login
-  attribute_writer :swipecard_code
+
+  #TODO make swipecard_code readonly. Can't at the moment because of a bug
+  #attribute_writer :swipecard_code
+  attribute_accessor :swipecard_code
+
   attribute_reader :has_a_swipecard_code
 end
