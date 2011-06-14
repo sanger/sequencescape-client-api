e5c160f9734447b631bbd16533bf57bfdc8daee9
Matthew Denner <md12@sanger.ac.uk>
2011-06-14 11:52:53 +0100

    Enable grouping of attributes.
    Samples have a highly structured JSON from the API and this commit
    provides a convenience to that structure so that the code can use dotted
    notation (sample.sanger.name) rather than hash based versions
    (sample.sanger['name']).
    
    This has only been checked for read-only abilities, which is what is
    needed to support the pulldown pipeline.

---
+++ lib/sequencescape-api/resource/attribute_groups.rb
@@ -0,0 +1,74 @@
+require 'active_model/dirty'
+
+module Sequencescape::Api::Resource::Groups
+  module InstanceMethods
+    def attribute_groups
+      @attribute_groups ||= {}
+    end
+
+    def attribute_group_json(options)
+      Hash[attribute_groups.map { |k,v| [ k.to_s, v.send(:as_json_for_update, options) ] if v.changed? }.compact]
+    end
+    private :attribute_group_json
+
+    def changed?
+      super or attribute_groups.values.any?(&:changed?)
+    end
+
+    def clear_changed_attributes
+      super
+      attribute_groups.values.map(&:clear_changed_attributes)
+    end
+  end
+
+  module Json
+    def as_json_for_update(options)
+      super.tap { |json| json[json_root].merge!(attribute_group_json(options)) }
+    end
+    private :as_json_for_update
+  end
+
+  def self.extended(base)
+    base.class_eval do
+      include InstanceMethods
+    end
+  end
+
+  def attribute_group(name, &block)
+    proxy_class = Class.new(Proxy)
+    proxy_class.instance_eval(&block)
+    define_method(name)       {              attribute_groups[name.to_sym] ||= proxy_class.new(self) }
+    define_method("#{name}=") { |attributes| attribute_groups[name.to_sym]   = proxy_class.new(self, attributes) }
+  end
+end
+
+class Sequencescape::Api::Resource::Groups::Proxy
+  module InstanceMethods
+    def self.included(base)
+      base.class_eval do
+        attr_reader :attributes
+        private :attributes
+      end
+    end
+
+    def initialize(owner, attributes = {})
+      @owner, @attributes = owner, {}
+      attributes.each { |k,v| send(:"#{k}=", v) if respond_to?(:"#{k}=") }
+    end
+
+    def as_json_for_update(options)
+      attribute_group_json(options).merge(Hash[changes.map { |k,(_,v)| [k.to_s, v] }])
+    end
+    private :as_json_for_update
+
+    def clear_changed_attributes
+      changed_attributes.clear
+    end
+    private :clear_changed_attributes
+  end
+
+  include InstanceMethods
+  include ::ActiveModel::Dirty
+  extend Sequencescape::Api::Resource::Attributes
+  extend Sequencescape::Api::Resource::Groups
+end
