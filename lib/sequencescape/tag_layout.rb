a02fa540dfc18a3aeaf1e3a8635c1c627dbd113c
Matthew Denner <md12@sanger.ac.uk>
2011-06-21 15:38:53 +0100

    Pick up the direction for the tag layout & template.

--- lib/sequencescape/tag_layout.rb
+++ lib/sequencescape/tag_layout.rb
@@ -4,4 +4,6 @@ require 'sequencescape/tag'
 class Sequencescape::TagLayout < ::Sequencescape::Api::Resource
   belongs_to :plate, :class_name => 'Plate'
   composed_of :tag_group, :class_name => 'Tag::Group'
+
+  attr_reader :direction
 end
--- lib/sequencescape/tag_layout_template.rb
+++ lib/sequencescape/tag_layout_template.rb
@@ -2,7 +2,7 @@ require 'sequencescape-api/resource'
 require 'sequencescape/tag'
 
 class Sequencescape::TagLayoutTemplate < ::Sequencescape::Api::Resource
-  attribute_reader :name
+  attribute_reader :name, :direction
   belongs_to :plate, :class_name => 'Plate'
   composed_of :tag_group, :class_name => 'Tag::Group'
 
