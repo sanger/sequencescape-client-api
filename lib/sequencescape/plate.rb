7a92a77c09b99294c5f351516f8b74f10242e85c
Matthew Denner <md12@sanger.ac.uk>
2011-06-23 15:16:40 +0100

    Plates have source transfers now too.

--- lib/sequencescape/plate.rb
+++ lib/sequencescape/plate.rb
@@ -6,6 +6,8 @@ class Sequencescape::Plate < ::Sequencescape::Asset
 
   has_many :wells, :disposition => :inline
   belongs_to :plate_purpose
+
+  has_many :source_transfers, :class_name => 'Transfer'
   belongs_to :creation_transfer, :class_name => 'Transfer'
 
   attribute_accessor :size, :iteration, :state
