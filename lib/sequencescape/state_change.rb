9352aa288dcbfeb7df04377a7367cde0f4224e9c
Matthew Denner <md12@sanger.ac.uk>
2011-06-29 15:53:47 +0100

    Allow contents to be specified for state changes.

--- lib/sequencescape/state_change.rb
+++ lib/sequencescape/state_change.rb
@@ -2,6 +2,7 @@ require 'sequencescape-api/resource'
 
 class Sequencescape::StateChange < ::Sequencescape::Api::Resource
   belongs_to :target, :class_name => 'Asset'
+  attribute_accessor :contents          # Array of "contents" to fail, deciphered by the target, can be nil
   attribute_accessor :target_state
   attribute_accessor :previous_state
 end
