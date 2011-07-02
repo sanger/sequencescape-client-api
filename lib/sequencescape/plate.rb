c982ca3f28f1983c06ce1ff9dbce174b12859bb9
Matthew Denner <matt.denner@gmail.com>
2011-07-02 13:39:41 +0100

    Added stock plate to plates, which is need for pulldown.

--- lib/sequencescape/plate.rb
+++ lib/sequencescape/plate.rb
@@ -8,6 +8,7 @@ class Sequencescape::Plate < ::Sequencescape::Asset
 
   has_many :wells, :disposition => :inline
   belongs_to :plate_purpose
+  composed_of :stock_plate, :class_name => 'Plate'
 
   has_many :source_transfers, :class_name => 'Transfer'
   belongs_to :creation_transfer, :class_name => 'Transfer'
