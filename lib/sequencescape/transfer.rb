ab10f01d8d45509ada1f8fc5a560dd6dcee488e8
Matthew Denner <md12@sanger.ac.uk>
2012-07-31 10:33:42 +0100

    [#33625797] Add targets for transfers

--- lib/sequencescape/transfer.rb
+++ lib/sequencescape/transfer.rb
@@ -5,4 +5,5 @@ class Sequencescape::Transfer < ::Sequencescape::Api::Resource
   belongs_to :source, :class_name => 'Asset', :disposition => :inline
   belongs_to :destination, :class_name => 'Asset', :disposition => :inline
   attribute_reader :transfers
+  attribute_accessor :targets
 end
