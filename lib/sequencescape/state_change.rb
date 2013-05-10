ef56903dfd252508aae8cde7f067ebf9001633c2
James Glover <jg16@sanger.ac.uk>
2013-05-10 14:39:46 +0100

    State change has assets inline

--- lib/sequencescape/state_change.rb
+++ lib/sequencescape/state_change.rb
@@ -2,7 +2,7 @@ require 'sequencescape-api/resource'
 
 class Sequencescape::StateChange < ::Sequencescape::Api::Resource
   belongs_to :user
-  belongs_to :target, :class_name => 'Asset'
+  belongs_to :target, :class_name => 'Asset', :disposition => :inline
   attribute_accessor :contents          # Array of "contents" to fail, deciphered by the target, can be nil
   attribute_accessor :target_state
   attribute_accessor :previous_state
