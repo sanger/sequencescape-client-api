eff405b00ef784fe88f5b968d3aaa87ed998e82b
James Glover <james.glover@sanger.ac.uk>
2014-02-25 10:30:40 +0000

    Update lot and qc models to include relevant info

--- lib/sequencescape/qcable.rb
+++ lib/sequencescape/qcable.rb
@@ -7,7 +7,11 @@ class Sequencescape::Qcable < ::Sequencescape::Api::Resource
   belongs_to :qcable_creator
   belongs_to :asset
 
-  attribute_accessor :asset_barcode, :state
+  attribute_accessor :state, :stamp_bed, :stamp_index
 
+  attribute_group :barcode do
+    attribute_accessor :prefix, :number     # The pieces that make up a barcode
+    attribute_accessor :ean13               # The EAN13 barcode number
+  end
 
 end
