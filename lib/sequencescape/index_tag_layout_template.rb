c31af39741736299ceca40301380ae9a19e280f7
James Glover <james.glover@sanger.ac.uk>
2015-05-13 14:18:05 +0100

    Add index_tag_layout_template

---
+++ lib/sequencescape/index_tag_layout_template.rb
@@ -0,0 +1,9 @@
+require 'sequencescape-api/resource'
+require 'sequencescape/tag'
+
+class Sequencescape::IndexTagLayoutTemplate < ::Sequencescape::Api::Resource
+  attribute_reader :name,
+  composed_of :tag, :class_name => 'Tag'
+
+  # has_create_action :resource => 'index_tag_layout'
+end
