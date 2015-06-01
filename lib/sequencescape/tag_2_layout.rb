4197a46324d70a7322fbf6f86bfb5619a613d232
James Glover <james.glover@sanger.ac.uk>
2015-06-01 11:41:06 +0100

    [#94395714] Rename index_tag to tag_2

--- lib/sequencescape/tag_2_layout.rb
+++ lib/sequencescape/tag_2_layout.rb
@@ -1,7 +1,7 @@
 require 'sequencescape-api/resource'
 require 'sequencescape/tag'
 
-class Sequencescape::IndexTagLayout < ::Sequencescape::Api::Resource
+class Sequencescape::Tag2Layout < ::Sequencescape::Api::Resource
   belongs_to :user
   belongs_to :plate, :class_name => 'Plate'
   composed_of :tag, :class_name => 'Tag'
--- lib/sequencescape/tag_2_layout_template.rb
+++ lib/sequencescape/tag_2_layout_template.rb
@@ -1,9 +1,9 @@
 require 'sequencescape-api/resource'
 require 'sequencescape/tag'
 
-class Sequencescape::IndexTagLayoutTemplate < ::Sequencescape::Api::Resource
+class Sequencescape::Tag2LayoutTemplate < ::Sequencescape::Api::Resource
   attribute_reader :name
   composed_of :tag, :class_name => 'Tag'
 
-  has_create_action :resource => 'index_tag_layout'
+  has_create_action :resource => 'tag_2_layout'
 end
