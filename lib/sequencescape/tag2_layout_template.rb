8a2234abf6f256fabeca76a5fc2bcec81bf6c6b3
James Glover <james.glover@sanger.ac.uk>
2015-06-01 15:21:31 +0100

    Use tag2 not tag_2

--- lib/sequencescape/tag2_layout_template.rb
+++ lib/sequencescape/tag2_layout_template.rb
@@ -5,5 +5,5 @@ class Sequencescape::Tag2LayoutTemplate < ::Sequencescape::Api::Resource
   attribute_reader :name
   composed_of :tag, :class_name => 'Tag'
 
-  has_create_action :resource => 'tag_2_layout'
+  has_create_action :resource => 'tag2_layout'
 end
