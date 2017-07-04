40ebf1e91913cf115278fef2f0802b50559c1704
James Glover <james.glover@sanger.ac.uk>
2017-07-04 17:03:08 +0100

    Expose submission on transfer request

--- lib/sequencescape/transfer_request.rb
+++ lib/sequencescape/transfer_request.rb
@@ -4,6 +4,7 @@ class Sequencescape::TransferRequest < ::Sequencescape::Api::Resource
 
   belongs_to :source_asset, :class_name => 'Asset'
   belongs_to :target_asset, :class_name => 'Asset'
+  belongs_to :submission, :class_name => 'Submission'
 
   attribute_accessor :type, :state
 
