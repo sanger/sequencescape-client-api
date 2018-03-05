d1194c3e14937f760e7a234688d1c94d63be45d3
James Glover <james.glover@sanger.ac.uk>
2018-03-05 16:22:06 +0000

    Add used_tag_layout_templates to submission pool

--- lib/sequencescape/submission_pool.rb
+++ lib/sequencescape/submission_pool.rb
@@ -1,9 +1,7 @@
 require 'sequencescape-api/resource'
 
 class Sequencescape::SubmissionPool < ::Sequencescape::Api::Resource
-
   attribute_reader :plates_in_submission
   attribute_reader :used_tag2_layout_templates
-
-
+  attribute_reader :used_tag_layout_templates
 end
