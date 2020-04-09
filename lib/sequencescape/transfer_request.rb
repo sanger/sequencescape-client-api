e934b023d186394e751b0834a93502f489ce3b04
Eduardo Martin Rojo <emr@sanger.ac.uk>
2020-04-09 11:33:25 +0100

    Added new attribute for transfer_request merge_equivalent_aliquots

--- lib/sequencescape/transfer_request.rb
+++ lib/sequencescape/transfer_request.rb
@@ -7,5 +7,5 @@ class Sequencescape::TransferRequest < ::Sequencescape::Api::Resource
   belongs_to :submission, :class_name => 'Submission'
   belongs_to :outer_request, :class_name => 'Request'
 
-  attribute_accessor :type, :state, :submission_id, :volume
+  attribute_accessor :type, :state, :submission_id, :volume, :merge_equivalent_aliquots
 end
