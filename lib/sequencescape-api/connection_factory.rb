2ab14ddc7f755a2021b8d45ba960456945a9c5c7
James Glover <james.glover@sanger.ac.uk>
2021-07-21 09:47:26 +0100

    Rubocop

--- lib/sequencescape-api/connection_factory.rb
+++ lib/sequencescape-api/connection_factory.rb
@@ -8,7 +8,7 @@ class Sequencescape::Api::ConnectionFactory
   def self.create(options)
     required_options = []
     required_options.push(:user_api_key) if options[:authorisation].blank?
-    required_options.push(:url)    if default_url.blank?
+    required_options.push(:url) if default_url.blank?
 
     required_options.push(allow_blank: false)
     options.required!(*required_options) do |missing|
