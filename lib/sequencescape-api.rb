81a317bb5138f2d5dfd1860721b815ebd18ba9f5
James Glover <james.glover@sanger.ac.uk>
2021-07-28 11:05:25 +0100

    Relax active support requirements

--- lib/sequencescape-api.rb
+++ lib/sequencescape-api.rb
@@ -1,4 +1,5 @@
 require 'active_support'
+require 'active_support/core_ext/array/extract_options'
 require 'active_support/core_ext/module/delegation'
 require 'active_support/core_ext/class'
 require 'active_support/core_ext/kernel/singleton_class'
