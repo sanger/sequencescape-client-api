7cec373d6042714c8f7e582db455005c7acc19a1
Katy Taylor <kt17@sanger.ac.uk>
2021-08-16 16:46:27 +0100

    allow SpecificTubeCreation to have multple parents, to support sending through a SpikedBufferTube as a parent as well as a plate. Leave the old association for backwards compatibility:

--- lib/sequencescape/specific_tube_creation.rb
+++ lib/sequencescape/specific_tube_creation.rb
@@ -3,6 +3,7 @@ require 'sequencescape-api/resource'
 class Sequencescape::SpecificTubeCreation < ::Sequencescape::Api::Resource
   belongs_to :user
   belongs_to :parent, class_name: 'Plate'
+  has_many :parents, class_name: 'Asset'
   attribute_writer :child_purposes, :tube_attributes
   has_many :children, class_name: 'Tube'
 end
